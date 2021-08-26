dc341ffb60bdd2a4d61ad081f6dc0deeb79a2a5a
quanganh2001 <cocoi5659@gmail.com>
2021-08-26 23:20:45 +0700

    Biến trong Java

---
+++ Variable.java
@@ -0,0 +1,17 @@
+public class Variable {
+    public static void main(String[] args) {
+    String name;
+    float height;
+    int age;
+    name = "Quang Anh";
+    height = 1.7f;
+    age = 21;
+    // Dùng lệnh print thì không xuống dòng
+    System.out.print("Tên: ");
+    System.out.println(name);
+    System.out.print("Chiều cao: ");
+    System.out.println(height);
+    System.out.print("Tuổi: ");
+    System.out.println(age);
+    }
+}
