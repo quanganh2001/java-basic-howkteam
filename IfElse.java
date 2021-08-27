063330049a8e73ec187228df71e5a9029ee424be
quanganh2001 <cocoi5659@gmail.com>
2021-08-27 16:07:09 +0700

    Cấu trúc rẽ nhanh dạng đầy đủ

---
+++ IfElse.java
@@ -0,0 +1,10 @@
+public class IfElse{
+
+    public static void main(String []args){
+       int age = 18;
+       if (age > 18)
+       System.out.print("Bạn đủ tuổi để đăng ký");
+       else
+       System.out.print("Bạn chưa đủ tuổi để đăng ký");
+    }
+}
