10e51c02f93bb8a425c2f4ad4de0019db80b9426
quanganh2001 <cocoi5659@gmail.com>
2021-08-27 16:09:34 +0700

    Dạng if...else if...else trong Java

---
+++ ConditionFull.java
@@ -0,0 +1,11 @@
+public class ConditionFull{
+    public static void main(String []args){
+       int age = 20;
+       if (age >= 18) {
+       System.out.println("Bạn đủ tuổi để đăng ký");
+       System.out.println("Mời bạn đăng ký");
+       }
+       else
+       System.out.print("Bạn chưa đủ tuổi để đăng ký");
+    }
+}
