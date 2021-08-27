42c17c90cc37b47f53005bf11651c142463831b1
quanganh2001 <cocoi5659@gmail.com>
2021-08-27 11:04:37 +0700

    Ví dụ về sử dụng hằng trong Java

---
+++ PerimeterCircle.java
@@ -0,0 +1,7 @@
+public class PerimeterCircle {
+    public static void main(String[] args) {
+        final double PI = 3.1415926535897;
+        int r = 3;
+        System.out.println(2*r*PI);
+    }
+}
