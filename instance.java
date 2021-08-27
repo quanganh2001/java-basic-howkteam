de4fd779e679c1d66bb3cbf6104911061f8f83d6
quanganh2001 <cocoi5659@gmail.com>
2021-08-27 09:48:13 +0700

    Toán tử Instanceof

---
+++ instance.java
@@ -0,0 +1,7 @@
+public class instance {
+    public static void main (String[] args) {
+        String name = "HowKTeam";
+        Boolean isString = name instanceof String;
+        System.out.println(isString);
+    }
+}
