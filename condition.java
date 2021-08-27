2d886789e198a3e54611fa1948435322c3da8a18
quanganh2001 <cocoi5659@gmail.com>
2021-08-27 09:49:54 +0700

    Toán tử điều kiện trong Java

---
+++ condition.java
@@ -0,0 +1,8 @@
+public class condition {
+    public static void main(String[] args) {
+        int a = 1;
+        int b = 2;
+        int c = a<b ? 3 : 4;
+        System.out.println(c);
+    }
+}
