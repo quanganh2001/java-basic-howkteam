c549aded9478bff3079447f442500bd146c5d918
quanganh2001 <cocoi5659@gmail.com>
2021-08-28 08:34:58 +0700

    Khởi tạo cho mảng trong Java

---
+++ InitializeArray.java
@@ -0,0 +1,6 @@
+public class InitializeArray {
+    public static void main(String[] args) {
+        char[] a = {'H', 'o', 'w', 'K', 't', 'e', 'a', 'm'};
+        System.out.println(a);
+        }
+    }
