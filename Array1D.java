b15b07fac1b0acd19ebea64df4110b676aced9cc
quanganh2001 <cocoi5659@gmail.com>
2021-08-28 08:32:15 +0700

    Mảng một chiều trong Java

---
+++ Array1D.java
@@ -0,0 +1,13 @@
+public class Array1D {
+    public static void main(String[] args) {
+        int[] a;
+        a = new int[3];
+        a[0] = 5;
+        a[1] = 2;
+        a[2] = 1;
+        System.out.println(a);
+        for (int i = 0; i < a.length; i++) {
+            System.out.println(a[i]);
+        }
+    }
+}
