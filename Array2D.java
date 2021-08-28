16773fac3b04d51c882aa5d5e28d4975711e84fe
quanganh2001 <cocoi5659@gmail.com>
2021-08-28 08:39:05 +0700

    Mảng hai chiều trong Java

---
+++ Array2D.java
@@ -0,0 +1,9 @@
+public class Array2D {
+    public static void main(String[] args) {
+        int[][] a = {{1,2,3}, {4,5,6}, {7,8,9}};
+        for (int i = 0; i < 3; i++) {
+            System.out.println(a[i][i]+"");
+        }
+        System.out.println();
+    }
+}
