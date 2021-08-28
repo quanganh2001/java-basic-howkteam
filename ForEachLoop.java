ac1f19f68a6ec90d4c9b3215e8749f1c673b30b3
quanganh2001 <cocoi5659@gmail.com>
2021-08-28 22:20:13 +0700

    Vòng lặp For Each trong Java

---
+++ ForEachLoop.java
@@ -0,0 +1,9 @@
+public class ForEachLoop {
+    public static void main(String[] args) {
+        int[] array = {1,2,4};
+
+        for (int a: array) {
+            System.out.println(a);
+        }
+    }
+}
