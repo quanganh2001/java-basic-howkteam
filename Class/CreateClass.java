87d106d6245062b3bbdc20789416ed61ef1f6c77
quanganh2001 <cocoi5659@gmail.com>
2021-08-29 10:12:16 +0700

    Tạo class trong Java

---
+++ Class/CreateClass.java
@@ -0,0 +1,11 @@
+package Class;
+
+public class CreateClass {
+    public String name;
+	public int age;
+	public float height;
+	
+	public void eat() {
+		System.out.print("Person is eating");
+	}
+}
