de189b9c3a8d9103e639cdf4f4b032ed852bafba
quanganh2001 <cocoi5659@gmail.com>
2021-08-30 09:05:06 +0700

    Như vậy ở phương thức main, ta có thể khởi tạo đối tượng Person theo nhiều cách khác nhau:

---
+++ This_Keyword/HelloWorld.java
@@ -0,0 +1,10 @@
+package This_Keyword;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		Person a = new Person("Chau");
+		Person b = new Person("Chau", 21);
+		Person c = new Person("Chau", 21, 1.7f);
+	}
+}
