45847c345cdb42384c5b1b32c4fced1799b86218
quanganh2001 <89558595+quanganh2001@users.noreply.github.com>
2021-08-30 15:12:33 +0700

    Try Catch có nhiệm vụ bắt (Catch) các lỗi mà thực tế có thể xảy ra để xử lý sao cho chương trình thân thiện với người dùng hơn.

--- trycatch/totalarray.java
+++ trycatch/totalarray.java
@@ -1,5 +1,5 @@
 package trycatch;
-
+// Try Catch có nhiệm vụ bắt (Catch) các lỗi mà thực tế có thể xảy ra để xử lý sao cho chương trình thân thiện với người dùng hơn.
 public class totalarray {
     public static void main(String[] args) {
         int S = 0;
