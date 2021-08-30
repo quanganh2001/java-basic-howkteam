565bf2609863ff07ba2da227fd2e346bd72fcb46
quanganh2001 <cocoi5659@gmail.com>
2021-08-30 09:34:00 +0700

    Setter và Getter là 2 phương thức sử dụng để cập nhật hoặc lấy ra giá trị thuộc tính, đặc biệt dành cho các thuộc tính ở phạm vi private trong Java

---
+++ setter_getter/Person.java
@@ -0,0 +1,31 @@
+package setter_getter;
+
+public class Person {
+	
+	public String name;
+	private int age;
+	public float height;
+	
+	public Person(String name, int age, float height) {
+		this.name = name;
+		this.age = age;
+		this.height = height;
+	}
+	
+	public void setAge(int age) {
+		if (age>=0 && age<=100 ) {
+			this.age = age;
+		}
+	}
+	
+	public int getAge() {
+		return this.age;
+	}
+	
+	public void getInfo() {
+		System.out.println("Name:"+this.name);
+		System.out.println("Age:"+this.age);
+		System.out.println("Height:"+this.height);
+	}
+	
+}
